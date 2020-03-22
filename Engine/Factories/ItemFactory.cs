a4d5fc79c5735708591a2be78410cf567b0294ca
Zhen <zhen.yang3@outlook.com>
2020-03-22 14:34:16 -0700

    Adding monster element

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -21,6 +21,10 @@ namespace Engine.Factories
             _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
             _standardGameItems.Add(new GameItems(9001, "snake fang", 1));
             _standardGameItems.Add(new GameItems(9002, "Snakeskin", 2));
+            _standardGameItems.Add(new GameItems(9003, "rat tail", 1));
+            _standardGameItems.Add(new GameItems(9004, "Rat fur", 2));
+            _standardGameItems.Add(new GameItems(9005, "Spider fang", 1));
+            _standardGameItems.Add(new GameItems(9006, "Spider silk", 2));
         }
 
         public static GameItems CreateGameItem(int itemTypeID)
